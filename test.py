0b7a71275aca187146e9319ddd19707f07db82aa
jswon <mn2888@naver.com>
2017-05-12 16:05:05 +0900

    api version up

--- test.py
+++ test.py
@@ -1,28 +1,47 @@
 import vrep_python_mod
 from PIL import Image
 import time
+import datetime
 try :
-    env = vrep_python_mod.sim_env()
 
-#    joint_ang = env.get_joint()
+    print(datetime.datetime.now())
+    env = vrep_python_mod.sim_env(isrender=False)
 
-#    print(joint_ang)
+    tar_joint = [0, 0, 10, 10, 10, 0]
+    env.movejoint(tar_joint)
 
-    #env.movejoint([90, 0, 0, 90, 90, 0])
+    print('movejoint')
 
-#    img0 = env.get_cam_img(0)
+    print('target :{}'.format(tar_joint))
+    print('result :{}'.format(env.get_joint()))
 
-#    img0.show('test')
-    time.sleep(5)
+    img0 = env.get_cam_img(0)
+    img0.show('img_cut_0')
+
+    tar_joint = [0, 0, 20, 20, 20, 0]
+    env.movejoint(tar_joint)
+    img0 = env.get_cam_img(0)
+    img0.show('img_cut_1')
+    print('target :{}'.format(tar_joint))
+    print(env.get_joint())
+
+    tar_joint = [0, 0, 30, 30, 30, 0]
+    env.movejoint(tar_joint)
+    img0 = env.get_cam_img(0)
+    img0.show('img_cut_2')
+    print('target :{}'.format(tar_joint))
+    print('result :{}'.format(env.get_joint()))
+
     obj1 = env.get_obj_pos(0)
     print(obj1)
-    while True :
-        a =1
-    env.shutdown()
 
+    print(datetime.datetime.now())
+
+    while True:
+        pass
 
 except :
-    print("test")
+    print("non standard exit")
     env.shutdown()
     # TODO : Force quit  -> Call saver !!!
     # Function()
--- vrep_python_mod.py
+++ vrep_python_mod.py
@@ -18,6 +18,7 @@ import socket
 import numpy as np
 import sys
 from PIL import Image as Im
+import datetime
 
 
 
@@ -28,7 +29,7 @@ Shuffle_Count = 0
 shuffle_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 class sim_env():
-    def __init__(self):
+    def __init__(self, isrender=True):
         self.clientID = -1
         self.obj_handle = []
         self.rob_handle = []
@@ -37,203 +38,179 @@ class sim_env():
         self.cam_handle = []
         self.end_effector = []
         self.collision_handle = []
-        self.FLAG_CONNECT = False
-        # Sim_env Connect
+        self.isrender = isrender
+
+        # Simulatior Connect
         self.connect()
 
     def connect(self):
         print("Connecting to remote API server...", file=sys.stderr)
 
         lan_ip = socket.gethostbyname(socket.gethostname())
+
+        vrep.simxStopSimulation(-1, vrep.simx_opmode_oneshot_wait)
         vrep.simxFinish(-1)  # just in case, close all opened connections
+
         self.clientID = vrep.simxStart(lan_ip, 19997, True, True, 5000, 5)
-        self.FLAG_CONNECT = vrep.simxStartSimulation(self.clientID, vrep.simx_opmode_blocking)
 
-        if self.FLAG_CONNECT is not vrep.simx_return_ok :
-            print('Not Connected. Please confirm the simulation', file=sys.stderr)
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
 
-        else:
-            # UR3 Handle
-            err, self.rob_handle = vrep.simxGetObjectHandle(self.clientID, 'UR3', vrep.simx_opmode_oneshot_wait)
+        # Register all handles
+        print("Registering all handle of scene... (Rendering = {})".format(self.isrender), file=sys.stderr)
+        self.rob_handle = vrep.simxGetObjectHandle(self.clientID, 'UR3', vrep.simx_opmode_blocking)[1]
+        # Tray Handle
+        self.tray_handle = vrep.simxGetObjectHandle(self.clientID, 'Tray', vrep.simx_opmode_blocking)[1]
+        # End effector Handle
+        self.end_effector = vrep.simxGetObjectHandle(self.clientID, 'End_Effector', vrep.simx_opmode_blocking)[1]
+        # Object Handle
+        for i in obj_list:
+            self.obj_handle.append(vrep.simxGetObjectHandle(self.clientID, i, vrep.simx_opmode_blocking)[1])
+        # UR3 Joint Handle
+        for i in range(6):
+            self.joint_handle.append(vrep.simxGetObjectHandle(self.clientID, "UR3_joint{}".format(i + 1),
+                                                              vrep.simx_opmode_blocking)[1])
+        # Vision camera Handle
+        for i in range(2):
+            self.cam_handle.append(vrep.simxGetObjectHandle(self.clientID, 'Vision_sensor_0%d' % (i + 1),
+                                                            vrep.simx_opmode_blocking)[1])
 
-            # Tray Handle
-            err, self.tray_handle = vrep.simxGetObjectHandle(self.clientID, 'Tray', vrep.simx_opmode_oneshot_wait)
+        # Collision Handle
+        for i in range(14):
+            self.collision_handle.append(vrep.simxGetCollisionHandle(self.clientID, 'Collision%d' % i,
+                                                                     vrep.simx_opmode_blocking)[1])
 
-            # End effector Handle
-            err, self.end_effector = vrep.simxGetObjectHandle(self.clientID, 'End_Effector', vrep.simx_opmode_oneshot_wait)
+        vrep.simxStartSimulation(self.clientID, vrep.simx_opmode_oneshot_wait)
 
-            # Object Handle
-            for i in obj_list:
-                err, handle = vrep.simxGetObjectHandle(self.clientID, i, vrep.simx_opmode_oneshot_wait)
-                self.obj_handle.append(handle)
+        # Is Render?
+        err = vrep.simxSetBooleanParameter(self.clientID, vrep.sim_boolparam_display_enabled, self.isrender,
+                                           vrep.simx_opmode_oneshot)
 
-            # UR3 Joint Handle
-            for i in range(6):
-                err, joint_handle = vrep.simxGetObjectHandle(self.clientID, "UR3_joint{}".format(i+1),
-                                                             vrep.simx_opmode_oneshot_wait)
-                self.joint_handle.append(joint_handle)
+        # Data streaming
+        self.data_streaming()
 
-            # Vision camera Handle
-            for i in range(2):
-                err, cam = vrep.simxGetObjectHandle(self.clientID, 'Vision_sensor_0%d' %(i+1), vrep.simx_opmode_oneshot_wait)
-                self.cam_handle.append(cam)
-
-            # Collision Handle
-            for i in range(14):
-                err, collision = vrep.simxGetCollisionHandle(self.clientID, 'Collision%d' % i, vrep.simx_opmode_oneshot_wait)
-                self.collision_handle.append(collision)
-
-            # Start Simulation
-            vrep.simxStartSimulation(self.clientID, vrep.simx_opmode_oneshot)
-
-            # Data streaming
-            self.data_streaming()
-
-            print("Environment ready", file=sys.stderr)
-
+        print(datetime.datetime.now(), "  Complete registered, Robot ready", file=sys.stderr)
 
     def shutdown(self):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-        else:
-            self.data_streaming(option=False)
-            vrep.simxStopSimulation(self.clientID, vrep.simx_opmode_oneshot)
-            time.sleep(1)
-            vrep.simxFinish(self.clientID)
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
+
+        #self.data_streaming(option=False) # Need??
+        vrep.simxStopSimulation(self.clientID, vrep.simx_opmode_oneshot)
+        time.sleep(1)
+        vrep.simxFinish(self.clientID)
 
     def data_streaming(self, option = True):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-        else:
-            if option :
-                op = vrep.simx_opmode_streaming
-            else :
-                op = vrep.simx_opmode_discontinue
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
 
-            #Object postion
-            for i in self.obj_handle:
-                vrep.simxGetObjectPosition(self.clientID, i, self.rob_handle, op)
+        if option :
+            op = vrep.simx_opmode_streaming
+        else :
+            op = vrep.simx_opmode_discontinue
 
-            #Dummy position
-            vrep.simxGetObjectPosition(self.clientID, self.end_effector, self.rob_handle, op)
-
-            #vision
-            for i in self.cam_handle :
-                vrep.simxGetVisionSensorImage(self.clientID, i, 0, op)
-            #collision
-            for i in self.collision_handle:
-                vrep.simxReadCollision(self.clientID, i, op)
-            #UR3 Joint
-            for i in self.joint_handle:
-                vrep.simxGetJointPosition(self.clientID, i, op)
-
+        #Object postion
+        for i in self.obj_handle:
+            vrep.simxGetObjectPosition(self.clientID, i, self.rob_handle, op)
+        #Dummy position
+        vrep.simxGetObjectPosition(self.clientID, self.end_effector, self.rob_handle, op)
+        #vision
+        for i in self.cam_handle :
+            vrep.simxGetVisionSensorImage(self.clientID, i, 0, op)
+        #collision
+        for i in self.collision_handle:
+            vrep.simxReadCollision(self.clientID, i, op)
+        #UR3 Joint
+        for i in self.joint_handle:
+            vrep.simxGetJointPosition(self.clientID, i, op)
 
     def get_obj_pos(self, obj_idx): # only for 1 objects
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
 
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
+        if obj_idx in range(len(self.obj_handle)):
+            print('objGETPOS')
+            return vrep.simxGetObjectPosition(self.clientID, self.obj_handle[obj_idx], self.rob_handle,
+                                              vrep.simx_opmode_buffer)
+
         else:
-            if obj_idx in range(len(self.obj_handle)):
-                err, obj_pos = vrep.simxGetObjectPosition(self.clientID, self.obj_handle[obj_idx], self.rob_handle,
-                                                          vrep.simx_opmode_buffer)
-                return obj_pos
-
-
-            else:
-                raise ValueError("object index must be 0 ~ 9")
+            raise ValueError("object index must be 0 ~ 9")
 
 
 
 
     def shuffle(self):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-        else:
-            Past_number =[]
-            change_num = [-3, -2, -1, 1, 2, 3]
-            for i in self.obj_handle:
-                SufflePosition = []
-                L_x = 0.075 + 0.02*(2*random.random() - 1)
-                L_y_positive = (0.25 / 2) * (1 * random.random())
-                L_y_negative = (0.25 / 2) * (-1 * random.random())
-                L_z_positive = (0.70 / 2) * (1 * random.random())
-                L_z_negative = (0.70 / 2) * (-1 * random.random())
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
 
-                Suffle_part = random.randrange(1, 5)
-                if(Suffle_part == Past_number):
-                    Suffle_part = (Suffle_part + random.choice(change_num))%4
-                Past_number = Suffle_part
+        Past_number =[]
+        change_num = [-3, -2, -1, 1, 2, 3]
+        for i in self.obj_handle:
+            SufflePosition = []
+            L_x = 0.075 + 0.02*(2*random.random() - 1)
+            L_y_positive = (0.25 / 2) * (1 * random.random())
+            L_y_negative = (0.25 / 2) * (-1 * random.random())
+            L_z_positive = (0.70 / 2) * (1 * random.random())
+            L_z_negative = (0.70 / 2) * (-1 * random.random())
+
+            Suffle_part = random.randrange(1, 5)
+            if(Suffle_part == Past_number):
+                Suffle_part = (Suffle_part + random.choice(change_num))%4
+            Past_number = Suffle_part
 
 
-                if (Suffle_part == 1):
-                    SufflePosition.append(L_x)
-                    SufflePosition.append(L_y_positive)
-                    SufflePosition.append(L_z_positive)
-                elif (Suffle_part == 2):
-                    SufflePosition.append(L_x)
-                    SufflePosition.append(L_y_negative)
-                    SufflePosition.append(L_z_positive)
-                elif (Suffle_part == 3):
-                    SufflePosition.append(L_x)
-                    SufflePosition.append(L_y_positive)
-                    SufflePosition.append(L_z_negative)
-                else:
-                    SufflePosition.append(L_x)
-                    SufflePosition.append(L_y_negative)
-                    SufflePosition.append(L_z_negative)
+            if (Suffle_part == 1):
+                SufflePosition.append(L_x)
+                SufflePosition.append(L_y_positive)
+                SufflePosition.append(L_z_positive)
+            elif (Suffle_part == 2):
+                SufflePosition.append(L_x)
+                SufflePosition.append(L_y_negative)
+                SufflePosition.append(L_z_positive)
+            elif (Suffle_part == 3):
+                SufflePosition.append(L_x)
+                SufflePosition.append(L_y_positive)
+                SufflePosition.append(L_z_negative)
+            else:
+                SufflePosition.append(L_x)
+                SufflePosition.append(L_y_negative)
+                SufflePosition.append(L_z_negative)
 
-                vrep.simxSetObjectPosition(self.clientID, i, self.tray_handle, SufflePosition, vrep.simx_opmode_oneshot)
-                time.sleep(0.05)
+            vrep.simxSetObjectPosition(self.clientID, i, self.tray_handle, SufflePosition, vrep.simx_opmode_oneshot)
+            time.sleep(0.05)
 
     def movejoint(self, degree):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-            sys.exit()
-        elif len(degree) != 6 :
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
+
+        if len(degree) != 6:
             raise ValueError("The input's length not matched the number of joint")
-        else:
+
+        else :
             for idx, deg in enumerate(degree):
                 target_joint_rad = math.radians(deg)
                 vrep.simxSetJointTargetPosition(self.clientID, self.joint_handle[idx], target_joint_rad,
                                                 vrep.simx_opmode_blocking)
 
     def get_cam_img(self, cam_idx):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-            sys.exit()
-        else:
-           if cam_idx in [0, 1]:
-               while (vrep.simxGetConnectionId(self.clientID) != -1):
-                   err, resolution, image = vrep.simxGetVisionSensorImage(self.clientID, self.cam_handle[cam_idx], 0,
-                                                                          vrep.simx_opmode_buffer)
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
 
-                   # Save image to txt file => But it have a problem
-                   if err == vrep.simx_return_ok:
-                       # save image information list
-                       #print('vision information imported')
-                       image_byte_array = np.asarray(image).astype('uint8')
-                       result_image = Im.frombuffer("RGB", (resolution[0], resolution[1]), image_byte_array, "raw", "RGB", 0, 1)
-                       return result_image
+        if cam_idx not in [0, 1]:
+            raise ValueError("camera_idx must be 0 or 1")
 
-                   elif err == vrep.simx_return_novalue_flag:
-                       print("no vision information")
-                   else:
-                       print('Error')
-                       print(err)
-           else:
-               raise ValueError("camera_idx must be 0 or 1")
+        # TODO : image PIL.IMage????
+        err, resolution, image = vrep.simxGetVisionSensorImage(self.clientID, self.cam_handle[cam_idx], 0,
+                                                          vrep.simx_opmode_buffer)
+
+        image_byte_array = np.asarray(image).astype('uint8')
+        result_image = Im.frombuffer("RGB", (resolution[0], resolution[1]), image_byte_array, "raw", "RGB", 0, 1)
+
+        return result_image
 
     def get_joint(self):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-            sys.exit()
-        else:
-            cur_joint = []
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
 
-            for i in self.joint_handle:
-                ret, rad_joint = vrep.simxGetJointPosition(self.clientID, i, vrep.simx_opmode_buffer)
-                cur_joint.append(math.degrees(rad_joint))
-            return cur_joint # return as degree value
+        cur_joint = []
+
+        for i in self.joint_handle:
+            _, rad_joint = vrep.simxGetJointPosition(self.clientID, i, vrep.simx_opmode_buffer)
+            cur_joint.append(math.degrees(rad_joint))
+
+        return cur_joint # return as degree value
 
     # def collsion_Read(self):
     #     if self.FLAG_CONNECT is not vrep.simx_return_ok:
@@ -263,22 +240,21 @@ class sim_env():
     #         print('No Collision')
     #         return 0
 
-    def render(self, isrender = True):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
-            print('Not Connected. Please check the simulator', file=sys.stderr)
-            sys.exit()
-        else:
-            err = vrep.simxSetBooleanParameter(self.clientID, vrep.sim_boolparam_display_enabled, isrender, vrep.simx_opmode_oneshot)
-
     def remove_obj(self):
-        if self.FLAG_CONNECT is not vrep.simx_return_ok:
+        assert self.clientID != -1, 'Failed to connect to the V-rep server'
+
+        if self.clientID != -1 :
             print('Not Connected. Please check the simulator', file=sys.stderr)
 
         elif not shuffle_num:
             for i in range(10):
                 shuffle_num.append(i)
+
         random_num = random.randrange(0, len(shuffle_num))
         vrep.simxSetObjectPosition(self.clientID, self.obj_handle[shuffle_num[random_num]],
                                    self.rob_handle, [0.8, 0, 0], vrep.simx_opmode_oneshot)
 
         del shuffle_num[random_num]
+
+    def send(self):
+        vrep.simxSynchronousTrigger(self.clientID)
